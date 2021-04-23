30dcee560e9845ca29e50b5c0c51c14bdaa03e91
Shihaam Abdul Rahman <github@shihaam.me>
2021-04-24 01:46:34 +0500

    added colors

--- bml-bash/bml.sh
+++ bml-bash/bml.sh
@@ -3,11 +3,36 @@
 source .env
 
 BML_URL='https://www.bankofmaldives.com.mv/internetbanking/api'
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
 
-curl -s -c /tmp/bmlcookie $BML_URL/login \
+LOGIN=$(curl -s -c /tmp/bmlcookie $BML_URL/login \
 	--data-raw username=$BML_USERNAME \
 	--data-raw password=$BML_PASSWORD \
-	--compressed > /dev/null
+	--compressed \
+		| awk -F 'success":' '{print $2}' \
+		| cut -f1 -d ',' )
+
+if [ "$LOGIN" = "true" ]
+        then
+                NAME=$(curl -s -b /tmp/bmlcookie $BML_URL/profile \
+			| awk -F 'fullname":"' '{print $2}' \
+			| cut -f1 -d '"')
+		echo ""
+		echo ${green}Welcome ${reset}$NAME
+		echo ""
+elif [ "$LOGIN" = "false" ]
+        then
+                echo "${red}Username or Password incorrect" 1>&2
+                rm /tmp/bmlcookies 2> /dev/null
+                exit
+else
+                echo "${red}Unknown Error" 1>&2
+                rm /tmp/bmlcookies 2> /dev/null
+                exit
+fi
+
 echo "Menu"
 echo ""
 echo "1 - Accounts"
@@ -18,15 +43,14 @@ echo ""
 printf 'Please Input: '
 read -r INPUT1
 
-
 if [ "$INPUT1" = "1" ]
 	then
-		curl -s -c /tmp/bmlcookie $BML_URL/dashboard | jq
+		curl -s -b /tmp/bmlcookie $BML_URL/dashboard | jq
 elif [ "$INPUT1" = "2" ]
 	then
 		curl -s -b /tmp/bml/cookie $BML_URL/contacts | jq
 else
-	echo "There was an error"
+	echo "${red}There was an error"
 fi
 
 #curl -s -b /tmp/bmlcookie $BML_URL/contacts | jq
