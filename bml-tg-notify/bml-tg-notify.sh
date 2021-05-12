636512cbfc768363d2cb22ff59d461503fa1fb74
Shihaam Abdul Rahman <github@shihaam.me>
2021-05-12 20:17:26 +0500

    change delay without restarting script

--- bml-tg-notify/bml-tg-notify.sh
+++ bml-tg-notify/bml-tg-notify.sh
@@ -4,7 +4,7 @@ mkdir -p ~/.cache/bml-cli/
 TG_BOTAPI='https://api.telegram.org/bot'
 BML_URL='https://www.bankofmaldives.com.mv/internetbanking/api'
 COOKIE=~/.cache/bml-cli/cookie
-
+SLEEP=120
 while true; do
 LOGIN=$(curl -s -c $COOKIE $BML_URL/login --data-raw username=$BML_USERNAME --data-raw password=${BML_PASSWORD} | jq -r .code)
 if [ "$LOGIN" = "0" ]
@@ -22,7 +22,7 @@ CHECKDIFF2=$(echo $HISTORY | wc -c)
 
 if [ "$CHECKDIFF1" = "$CHECKDIFF2" ]
 	then
-	echo nothing new..checking again
+	echo "nothing new..checking again in $SLEEP seconds"
 else
 	TRANSFERAMOUNT=$(echo $HISTORY | jq -r .amount | head -n1)
 	TRANFERFROM=$(echo $HISTORY | jq -r .narrative3 | head -n1)
@@ -31,5 +31,6 @@ else
 	TRANFERFROM=`echo "$TRANFERFROM" | sed "s/ /%20/g"`
 	curl -s $TG_BOTAPI$TG_BOT_TOKEN/sendMessage?chat_id=$TG_CHATID'&'text=From:%20$TRANFERFROM%0AMVR:%20$TRANSFERAMOUNT > /dev/null
 fi
+SLEEP=$(cat delay)
 sleep $SLEEP
 done
