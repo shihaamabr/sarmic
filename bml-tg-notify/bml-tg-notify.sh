eb79e10f4d306702d976832faefc05c0fb3ee9e7
Shihaam Abdul Rahman <github@shihaam.me>
2021-05-12 08:02:47 +0500

    litle experiment

--- bml-tg-notify/bml-tg-notify.sh
+++ bml-tg-notify/bml-tg-notify.sh
@@ -29,7 +29,7 @@ else
 	echo From: $TRANFERFROM
 	echo MVR: $TRANSFERAMOUNT
 	TRANFERFROM=`echo "$TRANFERFROM" | sed "s/ /%20/"`
-	curl -s ''${TG_BOTAPI}''${TG_BOT_TOKEN}'/sendMessage?chat_id='${TG_CHATID}'&text=From:%20'${TRANFERFROM}%0A'MVR:%20'${TRANSFERAMOUNT}'' > /dev/null
+	curl -s ${TG_BOTAPI}${TG_BOT_TOKEN}/sendMessage?chat_id=${TG_CHATID}'&'text=From:%20${TRANFERFROM}%0AMVR:%20${TRANSFERAMOUNT} | jq
 fi
 sleep $SLEEP
 done
